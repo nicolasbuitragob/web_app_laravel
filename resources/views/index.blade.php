1750324ca9baea9cdfe08cb8436b88fc66f5fc1e
nicolasbuitragob <nicolasbuitragobautista@gmail.com>
2022-04-15 19:37:14 -0500

    initial layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class = "text-center">
+        <h1 class = "text-3xl text-gray-700 mb-2 uppercase">Latest courses</h1>
+        <h2 class = "text-xl text-gray-600">Professional education</h2>
+        <h3 class = "text-l text-gray-600"> Let's change the world!</h3>
+    </div>
+
+@endsection
