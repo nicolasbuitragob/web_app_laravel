b34dc89a0f061b1e353ebeb4146a89d8d22d8d45
nicolasbuitragob <nicolasbuitragobautista@gmail.com>
2022-04-23 12:50:31 -0500

    course lessons

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -12,6 +12,10 @@ class Course extends Model
     public function user(){
         return $this -> belongsTo(User::class);
     }
+
+    public function posts(){
+        return $this -> hasMany(Post::class);
+    }
     
     public function getExcerptAttribute()
     {
--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,17 +7,27 @@
                 <li class = "font-medium text-sm text-gray-400 uppercase mb-4">
                     Content
                 </li>
+                @foreach($course->posts as $post)
+                <li class = "flex items-center text-gray-600 mt-2">
+                    {{$post->name}}
+                    @if($post ->free)
+                        <span class = "text-xs text-gray-500 font-semibold bg-gray-300 px-2 rounded-full ml-auto">
+                            Gratis
+                        </span>
+                    @endif
+                </li>
+                @endforeach
             </ul>
         </div>
         <div class = "text-gray-700 col-span-2">
             <img src="{{ $course->image }}">
-            <h2 class = "text-4xl" > {{ $course->name }}</h2>
+            <h2 class  = "text-4xl" > {{ $course->name }}</h2>
             <p>{{ $course->description}} </p>
             <div class = "flex mt-3">
                 <img 
                     src="{{$course->user->avatar}}"
                     class = "h-10 w-10 rounded-full mr-2">
-
+            </div>
             <div>
                 <p class = "text-gray-500 text-sm">
                     {{$course->user->name}}
@@ -25,8 +35,7 @@
                 <p class = "text-xl text-gray-300 text-xs">
                     {{$course->created_at->diffForHumans()}}
                 </p>
-                </div>                
-            </div>
+            </div>                
         </div>
     </div>
 
